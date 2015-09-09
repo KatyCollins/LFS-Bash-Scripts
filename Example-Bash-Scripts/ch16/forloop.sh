ef4e25a8b1c325e8952a2a63395fadcaf96a4949
Katy Collins <katy.collins1@gmail.com>
2015-09-09 00:11:49 -0500

    Update forloop.sh

--- Example-Bash-Scripts/ch16/forloop.sh
+++ Example-Bash-Scripts/ch16/forloop.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 sum=0
+# values were originally 1 2 3 4 for the exercize
+# I wanted to see if it would change the echo command at the end
+# if it was done this way and it does show incorrectly the way it is coded
 for i in 1 5 3 6
 do
 	sum=$(($sum+$i))
